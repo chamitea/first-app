28ae7403984e8add9fc4a0bb7ab18eb6762ded66
Azaliya_PC <nerennium@gmail.com>
2023-06-18 01:18:53 +0300

    Add the fist functional

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastFilm1 = prompt('Один из последних просмотренных фильмов?',''),
+      scoreOfLastFilm1 = prompt('На сколько оцените его?',''),
+      lastFilm2 = prompt('Один из последних просмотренных фильмов?',''),
+      scoreOfLastFilm2 = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[lastFilm1] = scoreOfLastFilm1;
+personalMovieDB.movies[lastFilm2] = scoreOfLastFilm2;
+
+console.log(personalMovieDB);
